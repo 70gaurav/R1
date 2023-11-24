314d51bcb8ac09fd91212987225e7240f634a275
Gaurav <gk17265@gmail.com>
2023-11-24 15:00:31 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,15 @@ const About = () => {
                 <img 
                     src='https://avatars.githubusercontent.com/u/22561209?v=4' 
                     className='rounded-full w-[40%] m-auto'
-                    alt="Preethi Pantangi"
+                    alt="Gaurav Kumar"
                 />
                 <div className='text-center mt-2'>
-                    <div className='font-bold'>Sai Preethi Pantangi</div>
-                    <div>George Mason University</div>
-                    <div>Fairfax, Virginia</div>
+                    <div className='font-bold'>Gaurav kumar</div>
+                    <div>Fullstack developer</div>
+                    <div>Jaipur,India</div>
                 </div>
                 <div className="flex items-center justify-center mt-3">
-                    <a href="https://github.com/PreethiPantangi/" target="_blank" rel="noreferrer" className="mr-3">
+                    <a href="https://github.com/70gaurav/" target="_blank" rel="noreferrer" className="mr-3">
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                             alt="Github"
@@ -21,7 +21,7 @@ const About = () => {
                             height={30}
                         />
                     </a>
-                    <a href="https://linkedin.com/in/preethipantangi" target="_blank" rel="noreferrer" className="mr-3">
+                    <a href="https://linkedin.com/in/gaurav-kumar-webdev" target="_blank" rel="noreferrer" className="mr-3">
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"
                             alt="LinkedIn"
@@ -29,7 +29,7 @@ const About = () => {
                             height={30}
                         />
                     </a>
-                    <a href="https://pantangisaipreethi3.wixsite.com/portfolio" target="_blank" rel="noreferrer">
+                    <a href="https://vercel.com/70gaurav" target="_blank" rel="noreferrer">
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/686/686165.png"
                             alt="Portfolio"
